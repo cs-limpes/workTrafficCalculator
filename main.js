dbc5340b9ea4b5b800213769ee7616ea9f5756ee
Eddie <educatedsavage@educatedsavage.com>
2022-09-18 11:28:24 -0700

    adding to github

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ let leadsGoal = 0;
 function webTrafficGoal() {
       let var1 = document.querySelector("#ntlcalc").value
       let var2 = document.querySelector("#wtCR").value
-      document.querySelector("#wtrcalc").innerText = var1/var2;
+      document.querySelector("#wtrcalc").innerText = Number(var1/var2);
 }
 
  // total sales goal / total leads + conversion rate
 function totalLeadsGoal(){
       let var3 = document.querySelector("#wtrcalc").innerText.value
       let var4 = document.querySelector("#tlCR").value
-      document.querySelector("#ntlcalc").innerText = var3/var4;
+      document.querySelector("#ntlcalc").innerText = Number(var3/var4);
 }
