e3476a0d746a1037f0be202123cd565cb6f1ed36
Eddie <educatedsavage@educatedsavage.com>
2022-09-18 10:59:54 -0700

    added oninput

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let trafficGoal = 0;
+let leadsGoal = 0;
+// new total leads goal/conversion rate 1
+function webTrafficGoal() {
+      let var1 = document.querySelector("#ntlcalc").value
+      let var2 = document.querySelector("#wtCR").value
+      document.querySelector("#wtrcalc").innerText = var1/var2;
+}
+
+ // total sales goal / total leads + conversion rate
+function totalLeadsGoal(){
+      let var3 = document.querySelector("#wtrcalc").innerText.value
+      let var4 = document.querySelector("#tlCR").value
+      document.querySelector("#ntlcalc").innerText = var3/var4;
+}
